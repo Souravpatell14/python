cf6a8344957b9adb8b56510f7904a3f362a47e1b
Souravpatell14 <sourabhpatel1415@gmail.com>
2025-03-18 13:18:11 +0530

    Update if..else.py

--- if..else.py
+++ if..else.py
@@ -8,3 +8,16 @@ if(a>18):
    print("you can drive")
 else:
    print("you cannot drive")
+
+
+
+(2)applecast
+
+applecast=210
+budget =200
+
+if(applecast<=budget):
+   print("Alex, add 1. kg apple to the cast")
+else:
+   print("Alex, do not add 1. kg apple to the cast")
+
