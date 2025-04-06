dc19022b53d42dca99594315ef7d4150fc8e9e71
Souravpatell14 <sourabhpatel1415@gmail.com>
2025-04-06 18:06:39 +0530

    Update if..else.py

--- if..else.py
+++ if..else.py
@@ -21,3 +21,18 @@ if(applecast<=budget):
 else:
    print("Alex, do not add 1. kg apple to the cast")
 
+(3) percentage check
+
+
+marks1 = int(input("Enter Marks 1: "))
+marks2 = int(input("Enter Marks 2: "))
+marks3 = int(input("Enter Marks 3: "))
+
+# Calculate total percentage
+total_percentage = (marks1 + marks2 + marks3) * 100 / 300
+
+# Check if the student passed all subjects and the total percentage
+if marks1 >= 33 and marks2 >= 33 and marks3 >= 33:
+    print("You have passed with a percentage of:", total_percentage)
+else:
+    print("You have failed with a percentage of:", total_percentage)
