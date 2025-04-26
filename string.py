88ebae75561904127d78f52efca5a1526dbe78f6
Souravpatell14 <sourabhpatel1415@gmail.com>
2025-04-26 13:07:50 +0530

    Update string.py

--- string.py
+++ string.py
@@ -15,3 +15,28 @@ for i in s:
     if i == term:
         counter += 1  # Counter ko 1 se badha dete hain
         print('Frequency:', counter)  # Har baar match hone par frequency print karte hain
+
+
+
+(2) partical remove 
+
+# User se email input le rahe hain
+s = input('Enter the email: ')
+
+# User se remove karne ke liye ek character input le rahe hain
+term = input("What would you like to remove: ")
+
+# Nayi string banane ke liye ek khaali result string
+result = ''
+
+# Har character ke liye loop chalate hain
+for i in s:
+    # Agar current character remove karne wale term ke barabar nahi hai
+    if i != term:
+        result = result + i  # Character ko result string me add karte hain
+
+# Final string print karte hain
+print(result)
+
+
+
